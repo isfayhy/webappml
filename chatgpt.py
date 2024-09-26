ce6367edb856cf3b68391a629b40ed6b5833d550
isfayhy <imran.sfayhy@gmail.com>
2024-09-26 16:59:20 +0200

    a

---
+++ chatgpt.py
@@ -0,0 +1,72 @@
+import streamlit as st
+from openai import OpenAI
+
+
+st.title("Intégration Dall-E 3")
+OpenAIKEY = st.sidebar.text_input("Entrez votre clé API")
+st.write(OpenAIKEY)
+prompt = st.text_input("Entrez un texte pour générer une image")
+
+def prompt_dall_e():
+    client = OpenAI(api_key=OpenAIKEY)
+
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                "role": "system",
+                "content": f"Tu es un traducteur de langue qui traduit de l'anglais vers le français." +" Si le texte en entrée n'est pas en anglais, tu retournes la phrase : 'Je ne prends que du texte en anglais.'"
+                },
+
+                {
+                    "role": "user",
+                    "content": "Iphone 13 actualité"
+                },
+
+                {
+                    "role": "assistant",
+                    "content": "Sujet d'actu : Ils seront en vente à partir du 27 septembre 2021. L'iPhone 13, l'iPhone 13 Pro, l'iPhone 13 Pro Max et l'iPhone 13 Mini gardent le même design que leurs prédécesseurs. Cependant, l'iPhone 13 sera plus épais de 0.25 mm mais cela devrait passer presque inaperçu.",
+                },
+
+
+
+                {
+            "role": "user",
+            "content": "Hola todos",
+        },
+        {
+            "role": "assistant",
+            "content": "Je ne prends que du texte en anglais.",
+        },
+            {
+            "role": "user",
+            "content": "Bonjour tout le monde !",
+        },
+        {
+            "role": "assistant",
+            "content": "Je ne prends que du texte en anglais.",
+        },
+
+
+
+
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+            model="gpt-3.5-turbo",
+            temperature=0.3,
+        max_tokens=100,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+        )
+
+
+        # Réponse de ChapGPT
+        st.text(chat_completion.choices[0].message.content)
+    except Exception as e:
+        st.error(f"Erreur lors de la du texte {e}")
+if st.button("Générer"):
+    prompt_dall_e()
--- dalle-3.py
+++ dalle-3.py
@@ -3,13 +3,12 @@ from openai import OpenAI
 
 
 st.title("Intégration Dall-E 3")
-
-input_OpenAIKEY = st.sidebar.text_input("Entrez votre clé API")
-st.write(input_OpenAIKEY)
-prompt = st.text_input("Veuillez entrer un prompt pour créer une image")
+OpenAIKEY = st.sidebar.text_input("Entrez votre clé API")
+st.write(OpenAIKEY)
+prompt = st.text_input("Entrez un texte pour générer une image")
 
 def prompt_dall_e():
-    client = OpenAI(api_key=input_OpenAIKEY)
+    client = OpenAI(api_key=OpenAIKEY)
 
     try:
         response = client.images.generate(
@@ -20,9 +19,8 @@ def prompt_dall_e():
             n=1,
         )
         image_url = response.data[0].url
-        st.image(image_url, caption="Image générée par DALL·E")
+        st.image(image_url, caption="Votre image a été générée")
     except Exception as e:
         st.error(f"Erreur lors de la génération de l'image: {e}")
-
-if st.button("Générer l'image"):
+if st.button("Générer"):
     prompt_dall_e()
