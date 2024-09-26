62f1746ca06d14b40a82d84c8db50983f7bbdec1
isfayhy <imran.sfayhy@gmail.com>
2024-09-26 14:36:32 +0200

    Update home.py

--- home.py
+++ home.py
@@ -5,11 +5,11 @@ st.title("Mon formulaire")
 
 
 st.video("https://www.youtube.com/watch?v=4ch1oHamkv8")
-st.video("https://www.youtube.com/watch?v=ld-f9b3OSTg")
-
-if st.button("Première vidéo"):
+if st.button("Choisir"):
     st.success("Première vidéo choisie !")
 
-if st.button("Deuxieme vidéo"):
+st.video("https://www.youtube.com/watch?v=ld-f9b3OSTg")
+
+if st.button("Choisir"):
     st.success("Deuxième vidéo choisie !")
 
