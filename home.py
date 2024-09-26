ddbd460677580c19716c3c61fbcd7ce7766bd283
isfayhy <imran.sfayhy@gmail.com>
2024-09-26 14:35:57 +0200

    Update home.py

--- home.py
+++ home.py
@@ -4,4 +4,12 @@ st.set_page_config(page_title="WebApp", page_icon=":sunglasses:")
 st.title("Mon formulaire")
 
 
-st.text_input("Tapez votre nom")
+st.video("https://www.youtube.com/watch?v=4ch1oHamkv8")
+st.video("https://www.youtube.com/watch?v=ld-f9b3OSTg")
+
+if st.button("Première vidéo"):
+    st.success("Première vidéo choisie !")
+
+if st.button("Deuxieme vidéo"):
+    st.success("Deuxième vidéo choisie !")
+
