b0b8143dd3458517f5214686b140e38a59f82e0f
isfayhy <imran.sfayhy@gmail.com>
2024-09-26 14:21:56 +0200

    update

---
+++ dalle-3.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+client = OpenAI(
+  organization='org-RCxc2yLbmvEAtq0XTG0iWsZ7',
+  project='proj_WuUNsGfeqXKYHp1fMMLY8bmX',
+)
+
+stream = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
+
+        
