e7cef04e41e1b7d7e483994b6ae409c310a91e74
isfayhy <imran.sfayhy@gmail.com>
2024-09-26 15:48:19 +0200

    Update dalle-3.py

--- dalle-3.py
+++ dalle-3.py
@@ -7,17 +7,15 @@ st.title('Intégration OpenAI ')
 
 
 
-# Testez ici plusieurs variation du prompte
-
 APIKEY = st.text_input("Entre ta clé API :")
 
-client = OpenAI(api_key=APIKEY)
 
 prompt = st.text_input("Entre ton texte :") 
 
 
 
 if st.button("Générer"):
+    client = OpenAI(api_key=APIKEY)
     image = client.images.generate(
     model="dall-e-2",
     prompt=prompt,
