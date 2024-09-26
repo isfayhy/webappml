f20e0cbfc9e20d8c016bce528110a6bd03dae2d9
isfayhy <imran.sfayhy@gmail.com>
2024-09-26 14:58:14 +0200

    aaa

--- home.py
+++ home.py
@@ -4,4 +4,12 @@ st.set_page_config(page_title="WebApp", page_icon=":sunglasses:")
 st.title("Mon formulaire")
 
 
-st.video("https://www.youtube.com
+st.video("https://www.youtube.com/watch?v=4ch1oHamkv8")
+if st.button("Choisir"):
+    st.success("Première vidéo choisie !")
+
+st.video("https://www.youtube.com/watch?v=ld-f9b3OSTg")
+
+if st.button("Choisir"):
+    st.success("Deuxième vidéo choisie !")
+
