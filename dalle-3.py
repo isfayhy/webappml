6b6f46016079a5b7a77b670abfdf3d71d2d1e310
isfayhy <imran.sfayhy@gmail.com>
2024-09-26 16:12:05 +0200

    Update dalle-3.py

--- dalle-3.py
+++ dalle-3.py
@@ -2,31 +2,27 @@ import streamlit as st
 from openai import OpenAI
 
 
-st.title('Intégration OpenAI ')
+st.title("Intégration Dall-E 3")
 
+input_OpenAIKEY = st.text_input("Entrez votre clé API")
+st.write(input_OpenAIKEY)
+prompt = st.text_input("Veuillez entrer un prompt pour créer une image")
 
+def prompt_dall_e():
+    client = OpenAI(api_key=input_OpenAIKEY)
 
+    try:
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            #size="512x512",
+            quality="standard",
+            n=1,
+        )
+        image_url = response.data[0].url
+        st.image(image_url, caption="Image générée par DALL·E")
+    except Exception as e:
+        st.error(f"Erreur lors de la génération de l'image: {e}")
 
-OpenAIKEY = st.text_input("Entre ta clé API :")
-
-
-prompt = st.text_input("Entre ton texte :") 
-
-
-
-def generer() :
-    client = OpenAI(api_key=OpenAIKEY)
-    
-    image = client.images.generate(
-        model="dall-e-2",
-        prompt=prompt,
-        size="512x512",
-        quality="standard",
-        n=1,
-    )  
-    
-    image_url = image['data'][0]['url']
-    st.image(image_url)
-
-if st.button("Générer"):
-    generer()
+if st.button("Générer l'image"):
+    prompt_dall_e()
